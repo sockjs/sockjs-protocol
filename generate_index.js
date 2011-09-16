d205ed6a9c3011a3f459a8e4d826ececc7a76f17
Marek Majkowski <majek04@gmail.com>
2011-09-16 15:14:33 +0100

    Initial commit

---
+++ generate_index.js
@@ -0,0 +1,46 @@
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+
+var title = "SockJS protocol";
+var url = 'https://github.com/majek/sockjs-protocol';
+
+var p = console.log;
+
+function git_mtime(file, cb) {
+    var c = spawn('git', ['log','-1', '--pretty=format:%ai', file]);
+    var d = [];
+    c.stdout.on('data', function (data) {
+                    d.push( data.toString() );
+                });
+    c.on('exit', function () {
+             cb(d.join(''));
+         });
+}
+
+
+p('<!doctype html>');
+p('<html><head><title>'+title+'</title></head><body lang="en"><h1>'+title+'</h1><ul>');
+p('Source code: <a href="'+url+'"><b>'+url+'</b></a><hr><pre>');
+
+var end;
+
+fs.readdir(".", function(err, files) {
+               files.sort();
+               var run = function() {
+                   var file = files.shift();
+                   if (!file) {end(); return;}
+                   if (/sock.*html/.test(file)) {
+                       var tab = Array(40 - file.length).join(' ');
+                       var stat = fs.statSync(file);
+                       git_mtime(file, function (mtime) {
+                                     p('<a href="'+file+'">'+file+'</a>' + tab + mtime + '        ' + stat.size);
+                                     run();
+                                 });
+                   } else {run();}
+               };
+               run();
+           });
+
+end = function() {
+    p('</pre></body></html>');
+};
